489df90ff6f66231cbbad1f9d7d6ae107034c3b0
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-04-12 01:10:01 +0200

    feat(url): add support for route defined as string

--- tests/spec/amd.spec.js
+++ tests/spec/amd.spec.js
@@ -29,6 +29,28 @@ define([
 
       api.url('foo').should.eql('http://foo.domain.tld/api/foo');
     });
+
+    it('should support route as a string', function () {
+      var api = superapi.default({
+        baseUrl: 'http://foo.domain.tld/api',
+        services: {
+          foo: 'bar'
+        }
+      });
+
+      api.url('foo').should.eql('http://foo.domain.tld/api/bar');
+    });
+
+    it('should throw error if path is missing', function () {
+      var api = superapi.default({
+        baseUrl: 'http://foo.domain.tld/api',
+        services: {
+          foo: {}
+        }
+      });
+
+      api.url.bind(null, 'foo').should.throw();
+    });
   });
 
   describe.skip('request method', function () {
