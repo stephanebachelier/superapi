d68274c95c3b7e11a6d670e7968fec6c6bfc9560
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-04-12 00:25:24 +0200

    chore(karma): setup karma for test

---
+++ tests/test-main.js
@@ -0,0 +1,28 @@
+/* global requirejs */
+var tests = [];
+for (var file in window.__karma__.files) {
+  if (window.__karma__.files.hasOwnProperty(file)) {
+    if (/spec.js$/.test(file)) {
+      tests.push(file);
+    }
+  }
+}
+
+requirejs.config({
+  // Karma serves files from '/base'
+  baseUrl: '/base/dist',
+
+  shim: {},
+
+  paths: {
+    superagent: '../tests/vendor/superagent/superagent',
+    superapi: 'superapi.amd',
+  },
+
+  // ask Require.js to load these files (all our tests)
+  //deps: tests.concat(['jquery', 'underscore']),
+  deps: tests,
+
+  // start test run, once Require.js is done
+  callback: window.__karma__.start
+});
