b6e7e0142641fc06c500de0483fc0fb19562a05d
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-04-14 17:32:32 +0200

    chore(karma): switch to Chrome as PhantomJS
    - PhantomJS does not support Function.bind()
    - Fix coverage configuration

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
 
 
     preprocessors: {
-      'lib/{,*/}*.js': ['coverage']
+    'dist/{,*/}*.js': ['coverage']
     },
 
 
@@ -63,7 +63,7 @@ module.exports = function(config) {
     // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
     // - PhantomJS
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
 
 
     // If browser does not capture in given timeout [ms], kill it
