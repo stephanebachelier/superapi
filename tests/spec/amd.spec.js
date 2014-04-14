bb453cfbf0d3d9b6f00c84e9656e532f018e277d
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-04-14 17:01:10 +0200

    test(global configuration): add test to check that service options/headers are not overriden by global configuration

--- tests/spec/amd.spec.js
+++ tests/spec/amd.spec.js
@@ -233,4 +233,89 @@ define([
       api.request('foo')._header['x-requested-with'].should.eql('XMLHttpRequest');
     });
   });
+
+  describe('global options', function () {
+    it('should not override service option', function () {
+      var api = superapi.default({
+        baseUrl: 'http://foo.domain.tld/api',
+        services: {
+          foo: {
+            path: 'bar',
+            options: {
+              type: 'form'
+            }
+          }
+        },
+        options: {
+          type: 'json',
+        }
+      });
+
+      api.request('foo')._header.should.haveOwnProperty('content-type');
+      api.request('foo')._header['content-type'].should.eql('application/x-www-form-urlencoded');
+    });
+
+    it('should not override service header', function () {
+      var api = superapi.default({
+        baseUrl: 'http://foo.domain.tld/api',
+        services: {
+          foo: {
+            path: 'bar',
+            headers: {
+              'Content-type': 'application/x-www-form-urlencoded'
+            }
+          }
+        },
+        options: {
+          type: 'json'
+        }
+      });
+
+      api.request('foo')._header.should.haveOwnProperty('content-type');
+      api.request('foo')._header['content-type'].should.eql('application/x-www-form-urlencoded');
+    });
+  });
+
+  describe('global headers', function () {
+    it('should not override service option', function () {
+      var api = superapi.default({
+        baseUrl: 'http://foo.domain.tld/api',
+        services: {
+          foo: {
+            path: 'bar',
+            options: {
+              type: 'form'
+            }
+          }
+        },
+        headers: {
+          'Content-type': 'application/json',
+        }
+      });
+
+      api.request('foo')._header.should.haveOwnProperty('content-type');
+      api.request('foo')._header['content-type'].should.eql('application/x-www-form-urlencoded');
+    });
+
+    it('should not override service header', function () {
+      var api = superapi.default({
+        baseUrl: 'http://foo.domain.tld/api',
+        services: {
+          foo: {
+            path: 'bar',
+            headers: {
+              'Content-type': 'application/x-www-form-urlencoded'
+            }
+          }
+        },
+        headers: {
+          'Content-type': 'application/json',
+        }
+      });
+
+      api.request('foo')._header.should.haveOwnProperty('content-type');
+      api.request('foo')._header['content-type'].should.eql('application/x-www-form-urlencoded');
+    });
+  });
+
 });
