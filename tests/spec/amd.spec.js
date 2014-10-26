0c8e27fe8208f570e61ddc758d2f79a608a771ee
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-10-26 21:57:51 +0100

    test(query): add tests for new query string feature

--- tests/spec/amd.spec.js
+++ tests/spec/amd.spec.js
@@ -395,4 +395,41 @@ define([
     })
   });
 
+  describe.only('url query', function () {
+    it('buildUrlQuery - should create the query for an url', function () {
+      var api = superapi.default({});
+      var queryString = api.buildUrlQuery({
+        bar: 'user',
+        foo: 'john',
+        baz: 'doe'
+      });
+      queryString.should.eql('?bar=user&foo=john&baz=doe');
+    });
+
+    it('buildUrlQuery - should support empty', function () {
+      var api = superapi.default({});
+      var queryString = api.buildUrlQuery({});
+      queryString.should.eql('');
+    });
+
+    it('buildUrlQuery - should create the query for an url', function () {
+      var api = superapi.default({
+        baseUrl: 'http://example.tld',
+        services: {
+          foo: {
+            path:'/user/:foo.:bar.json'
+          }
+        }
+      });
+
+      var queryString = api.buildUrl('foo', {
+        foo: 'john',
+        bar: 'doe'
+      }, {
+        content: 'post',
+        since: '19700101'
+      });
+      queryString.should.eql('http://example.tld/user/john.doe.json?content=post&since=19700101');
+    });
+  });
 });
