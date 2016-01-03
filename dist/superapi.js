aa69827ff751cb86a7dd1b2fcdfbfd1f9a6be565
Stephane BACHELIER <stephane.bachelier@gmail.com>
2016-01-03 19:02:09 +0100

    Release 0.13.2

--- dist/superapi.js
+++ dist/superapi.js
@@ -398,31 +398,31 @@ define("superapi/service-handler",
        * - edit (function): callback to use, to tweak req if needed.
        */
       return function(options) {
+        options = options || {};
+        var data = options.data || {};
+        var params = options.params || {};
+        var query = options.query || {};
+        var callback = options.callback || null;
+        var edit = options.edit || null;
+
+        var req = this.request(sid, data, params, query, options.method);
+
+        // edit request if function defined
+        if (edit && "function" === typeof edit) {
+          edit(req);
+        }
+
+        // middleware response handler
+        var applyMiddlewares = this._applyMiddlewares(req, this.service(sid));
+
+        var result = function (resolver) {
+          return function (error, response) {
+            resolver(error, response);
+            applyMiddlewares(error, response);
+          }
+        }
+
         return new Promise(function (resolve, reject) {
-          options = options || {};
-          var data = options.data || {};
-          var params = options.params || {};
-          var query = options.query || {};
-          var callback = options.callback || null;
-          var edit = options.edit || null;
-
-          var req = this.request(sid, data, params, query, options.method);
-
-          // edit request if function defined
-          if (edit && "function" === typeof edit) {
-            edit(req);
-          }
-
-          // middleware response handler
-          var applyMiddlewares = this._applyMiddlewares(req, this.service(sid));
-
-          var result = function (resolver) {
-            return function (error, response) {
-              resolver(error, response);
-              applyMiddlewares(error, response);
-            }
-          }
-
           var failure = result(function (err, res) { reject(err); });
           var success = result(function (err, res) { resolve(res); });
 
